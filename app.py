dc570e5afb2ec36b1822ce53494dc5a147378bb9
JoonYong <joonyongpark99@gmail.com>
2024-12-08 22:47:23 +0900

    [FEAT] chian_bp 블루프린트 등록

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from app.routes.chian_routes_faiss import chian_bp
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+app.register_blueprint(chian_bp)
 
 @app.route("/")
 def hello():
